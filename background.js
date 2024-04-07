52b18ca12a08bba4f64ff37c642df3e3e332d4e6
Olivia Garrett <122964840+ogarrett1@users.noreply.github.com>
2024-04-06 19:45:57 -0700

    Update background.js

--- background.js
+++ background.js
@@ -6,27 +6,3 @@ chrome.runtime.onInstalled.addListener(() => {
 
 const extensions = 'https://developer.chrome.com/docs/extensions';
 const webstore = 'https://developer.chrome.com/docs/webstore';
-
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-
-    if (nextState === "ON") {
-      await chrome.scripting.insertCSS({
-        files: ["cute.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await chrome.scripting.removeCSS({
-        files: ["cute.css"],
-        target: { tabId: tab.id },
-      });
-    }
-  }
-});
